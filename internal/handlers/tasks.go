b6858cf57812a850bf5bd154c397a038afbee991
Dmitriy <dmitriy.tsaberyabyy@kuper.ru>
2026-01-05 19:30:19 +0300

    Do fmt

--- internal/handlers/tasks.go
+++ internal/handlers/tasks.go
@@ -2,8 +2,8 @@
 package handlers
 
 import (
-    "net/http"
-    "tasks-api/internal/storage"
+	"net/http"
+	"tasks-api/internal/storage"
 )
 
 type Handler struct{ Store storage.Storage }
@@ -12,10 +12,10 @@ func New(s storage.Storage) *Handler { return &Handler{Store: s} }
 
 // /tasks (GET, POST)
 func (h *Handler) TasksCollection(w http.ResponseWriter, r *http.Request) {
-    // TODO: реализуйте разбор метода, JSON, коды статусов, валидацию
+	// TODO: реализуйте разбор метода, JSON, коды статусов, валидацию
 }
 
 // /tasks/{id} (GET, PUT, DELETE)
 func (h *Handler) TaskItem(w http.ResponseWriter, r *http.Request) {
-    // TODO: извлечение id, маршрутизация по методу, ошибки
+	// TODO: извлечение id, маршрутизация по методу, ошибки
 }
--- internal/models/task.go
+++ internal/models/task.go
@@ -2,8 +2,8 @@
 package models
 
 type Task struct {
-	ID        int    `json:"id"` // генерируется на сервере
-	Title     string `json:"title"` // обязательное поле
-	Done      bool   `json:"done"` // статус выполнения
+	ID        int    `json:"id"`                   // генерируется на сервере
+	Title     string `json:"title"`                // обязательное поле
+	Done      bool   `json:"done"`                 // статус выполнения
 	CreatedAt string `json:"created_at,omitempty"` // ISO8601, по желанию
 }
